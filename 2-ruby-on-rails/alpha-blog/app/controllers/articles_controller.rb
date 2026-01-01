f700e30ed10d6ca2a364871f8749e468063d2cca
harsh-5401 <harshsingh5401@gmail.com>
2026-01-01 20:22:02 +0530

    Add new action and view for creating articles

--- 2-ruby-on-rails/alpha-blog/app/controllers/articles_controller.rb
+++ 2-ruby-on-rails/alpha-blog/app/controllers/articles_controller.rb
@@ -9,4 +9,16 @@ class ArticlesController < ApplicationController
     @articles=Article.all
   end
 
+  def new
+    # @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article]
+    @article=Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article
+    @article.save
+    redirect_to article_path(@article)
+  end
+
 end
